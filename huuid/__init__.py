db21274314d58daa44cd637e6e7d1930106483ca
Dirk <it@polarwinkel.de>
2025-02-25 21:33:54 +0100

    added documentation to code

--- huuid/__init__.py
+++ huuid/__init__.py
@@ -29,34 +29,44 @@ import math
 
 def __int32string(num):
     '''converts a 32bit-integer to a human-readable string'''
-    letters = ['b','d','f','g','h','j','k','l','m','n','p','r','s','t','v','z','B','C','D','F','G','H','J','K','L','M','N','P','R','S','T','V','Z'] # 33 options to start with (C is necessary to achieve 32bit)
+    letters = ['b','d','f','g','h','j','k','l','m','n','p','r','s','t',
+            'v','z','B','C','D','F','G','H','J','K','L','M','N','P','R',
+            'S','T','V','Z'] # 33 options to start with (C is necessary to achieve 32bit)
     vowels = ['a','e','i','o','u'] # 5 vowels
-    consonants = ['b','d','f','g','h','j','k','l','m','n','p','r','s','t','v','z'] # 16 phonetic-qunique consonants
+    consonants = ['b','d','f','g','h','j','k','l','m','n','p','r','s',
+            't','v','z'] # 16 phonetic-qunique consonants
     string = ''
     for i in range(9):
         if i == 0:
+            # first char can be capital or not + 'C' => 33 options
             j = num % 33
             num = num // 33
             string = string+letters[j]
         elif i % 3 == 1:
+            # vowel for 2nd, 5th and 8th char => 5 options
             j = num % 5
             num = num // 5
             string = string+vowels[j]
         else:
+            # non-capital consonant for others => 16 options
             j = num % 16
             num = num // 16
             string = string+consonants[j]
+    # 33*5*16*16*5*16*16*5*16 options > 2^32 (32bit)
     return string
 
 def __str2int32(s):
     '''converts a huuid-(sub-)string to a 32bit-integer'''
-    letters = ['b','d','f','g','h','j','k','l','m','n','p','r','s','t','v','z','B','C','D','F','G','H','J','K','L','M','N','P','R','S','T','V','Z'] # 33 options to start with (C is necessary to achieve 32bit)
+    letters = ['b','d','f','g','h','j','k','l','m','n','p','r','s','t',
+            'v','z','B','C','D','F','G','H','J','K','L','M','N','P','R',
+            'S','T','V','Z'] # 33 options to start with (C is necessary to achieve 32bit)
     vowels = ['a','e','i','o','u'] # 5 vowels
-    consonants = ['b','d','f','g','h','j','k','l','m','n','p','r','s','t','v','z'] # 16 phonetic-qunique consonants
-    out = consonants.index(s[8])
-    out = vowels.index(s[7])+out*5
-    out = consonants.index(s[6])+out*16
-    out = consonants.index(s[5])+out*16
+    consonants = ['b','d','f','g','h','j','k','l','m','n','p','r','s',
+            't','v','z'] # 16 phonetic-qunique consonants
+    out = consonants.index(s[8])        # tanslate first char to int
+    out = vowels.index(s[7])+out*5      # append 2nd char (5 options) to empty afte *5
+    out = consonants.index(s[6])+out*16 # append next 16 options (4bit => *16)
+    out = consonants.index(s[5])+out*16 # ...
     out = vowels.index(s[4])+out*5
     out = consonants.index(s[3])+out*16
     out = consonants.index(s[2])+out*16
@@ -70,7 +80,8 @@ def uuid2human(uid, depth=128):
         uid = uuid.UUID(uid)
     if not isinstance(uid, uuid.UUID):
         raise TypeError('not an uuid-object')
-    ustr = str(uid.hex)
+    ustr = str(uid.hex) # read uid as number
+    # convert to huuid-string depending on length:
     if depth == 32:
         return __int32string(int('0x'+ustr[0:8],16))
     elif depth == 64:
@@ -84,15 +95,17 @@ def human2uuid(s):
     '''convert a huuid-string back to a uuid-hex-string (short if <128bit)'''
     if not isinstance(s, str):
         raise TypeError('not a string')
-    ss = s.split('-')
+    ss = s.split('-') # split to 32bit-blocks ("words")
     out = ''
     for s in ss:
         if len(s) != 9:
             raise ValueError('not a huuid: all blocks must be composed of nine chars')
-        int32 = __str2int32(s)
-        out = out+f'0x{int32:08X}'[2:]
+        int32 = __str2int32(s) # convert huuid-block to int
+        out = out+f'0x{int32:08X}'[2:] # convert to hex
     if len(out) == 32:
+        # return as UUID-object if possible
         return uuid.UUID(out)
+    # as hex otherwise
     return out
 
 def new():
